788e10b57e4f7b3658baa853bdaa96b63e5f71fc
Svitlana_Potiichuk <ladys1813@gmail.com>
2025-05-02 16:10:32 +0300

    Changes

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,15 +1,17 @@
-const openBtn = document.querySelector('.open-menu-btn');
-const closeBtn = document.querySelector('.modal-close-btn');
-const modal = document.getElementById('mobile-menu');
+export function initBurgerMenu() {
+  const openBtn = document.querySelector('.open-menu-btn');
+  const closeBtn = document.querySelector('.modal-close-btn');
+  const modal = document.getElementById('mobile-menu');
 
-if (openBtn && closeBtn && modal) {
-  openBtn.addEventListener('click', () => {
-    modal.classList.add('is-open');
-    document.body.style.overflow = 'hidden';
-  });
+  if (openBtn && closeBtn && modal) {
+    openBtn.addEventListener('click', () => {
+      modal.classList.add('is-open');
+      document.body.style.overflow = 'hidden';
+    });
 
-  closeBtn.addEventListener('click', () => {
-    modal.classList.remove('is-open');
-    document.body.style.overflow = '';
-  });
+    closeBtn.addEventListener('click', () => {
+      modal.classList.remove('is-open');
+      document.body.style.overflow = '';
+    });
+  }
 }
