6b6434379f386e8360c95ce684e0ec77d2ab62fd
Svitlana Potiichuk <ladys1813@gmail.com>
2025-05-03 12:55:45 +0300

    Header

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -2,16 +2,23 @@ export function initBurgerMenu() {
   const openBtn = document.querySelector('.open-menu-btn');
   const closeBtn = document.querySelector('.modal-close-btn');
   const modal = document.getElementById('mobile-menu');
+  const header = document.querySelector('.header');
 
-  if (openBtn && closeBtn && modal) {
+  if (openBtn && closeBtn && modal && header) {
     openBtn.addEventListener('click', () => {
       modal.classList.add('is-open');
       document.body.style.overflow = 'hidden';
+      header.classList.add('menu-open');
+      header.classList.remove('menu-close');
+      document.body.classList.add('modal-blur');
     });
 
     closeBtn.addEventListener('click', () => {
       modal.classList.remove('is-open');
       document.body.style.overflow = '';
+      header.classList.remove('menu-open');
+      header.classList.add('menu-close');
+      document.body.classList.remove('modal-blur');
     });
   }
 }
