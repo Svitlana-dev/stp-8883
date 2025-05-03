6543e65d9982516675416e988f228a6bce7bc9b0
Svitlana Potiichuk <ladys1813@gmail.com>
2025-05-03 15:02:39 +0300

    New changes

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,24 +1,48 @@
 export function initBurgerMenu() {
-  const openBtn = document.querySelector('.open-menu-btn');
-  const closeBtn = document.querySelector('.modal-close-btn');
-  const modal = document.getElementById('mobile-menu');
-  const header = document.querySelector('.header');
+  const openBtn = document.querySelector('[data-open-menu]');
+  const closeBtn = document.querySelector('[data-close-menu]');
+  const modal = document.querySelector('[data-mobile-menu]');
+  const header = document.querySelector('[data-header]');
+  const body = document.body;
+  const modalLinks = document.querySelectorAll('[data-mobile-menu] a');
+
+  const closeMenu = () => {
+    delete modal.dataset.visible;
+    delete header.dataset.menu;
+    body.style.overflow = '';
+    delete body.dataset.blur;
+  };
 
   if (openBtn && closeBtn && modal && header) {
     openBtn.addEventListener('click', () => {
-      modal.classList.add('is-open');
-      document.body.style.overflow = 'hidden';
-      header.classList.add('menu-open');
-      header.classList.remove('menu-close');
-      document.body.classList.add('modal-blur');
+      modal.dataset.visible = 'open';
+      header.dataset.menu = 'open';
+      body.style.overflow = 'hidden';
+      body.dataset.blur = 'on';
     });
 
-    closeBtn.addEventListener('click', () => {
-      modal.classList.remove('is-open');
-      document.body.style.overflow = '';
-      header.classList.remove('menu-open');
-      header.classList.add('menu-close');
-      document.body.classList.remove('modal-blur');
+    closeBtn.addEventListener('click', closeMenu);
+
+    modalLinks.forEach(link => {
+      link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').slice(1);
+        const target = document.getElementById(targetId);
+        const headerOffset = header.offsetHeight;
+
+        if (target) {
+          const elementPosition =
+            target.getBoundingClientRect().top + window.scrollY;
+          const offsetPosition = elementPosition - headerOffset;
+
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+          });
+        }
+
+        closeMenu();
+      });
     });
   }
 }
