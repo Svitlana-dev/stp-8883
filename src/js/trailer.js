353fa929f8f99ad6308f73f0ec12b21faa04e6f6
tymur_lykho <tima.likho@gmail.com>
2025-05-04 17:18:23 +0300

    fix

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,6 +1,6 @@
 const video = document.getElementById('trailer');
 const desktopQuery = window.matchMedia('(min-width: 1200px)');
-const mobileQuery = window.matchMedia('(max-width: 320px)');
+const mobileQuery = window.matchMedia('(max-width: 1200px)');
 const playBtn = document.getElementById('customPlayBtn');
 
 playBtn.addEventListener('click', () => {
@@ -21,17 +21,17 @@ function checkScreen() {
     video.height = 500;
     video.width = 1128;
     if (window.devicePixelRatio > 2) {
-      video.setAttribute('poster', './img/trailer/video_desk@2x.jpg');
+      video.setAttribute('poster', '/img/trailer/video_desk@2x.jpg');
     } else {
-      video.setAttribute('poster', './img/trailer/video_desk@1x.jpg');
+      video.setAttribute('poster', '/img/trailer/video_desk@1x.jpg');
     }
   } else if (mobileQuery.matches) {
     video.height = 200;
     video.width = 288;
     if (window.devicePixelRatio > 2) {
-      video.setAttribute('poster', './img/trailer/video_mob@2x.jpg');
+      video.setAttribute('poster', '/img/trailer/video_mob@2x.jpg');
     } else {
-      video.setAttribute('poster', './img/trailer/video_mob@1x.jpg');
+      video.setAttribute('poster', '/img/trailer/video_mob@1x.jpg');
     }
   }
 }
